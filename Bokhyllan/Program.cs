5ca963c1396fd48fad84e19037155ea4313f6e39
robonexx <robertwagar@gmail.com>
2022-06-29 10:49:32 +0200

    changing the language to english, course uses swedish, but it kind of dont make sense

--- Bokhyllan/Program.cs
+++ Bokhyllan/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bokhyllan
+namespace BookShelf
+
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            List<Book> bookList = new List<Book>();
+
         }
     }
+
+    public abstract class Book
+    {
+        string Title;
+        string Author;
+        int YearOfPublication;
+        string Type;
+        bool InStock;
+
+        internal Book(string inputTitle, string inputAuthor, int inputYearOfPublication, bool inputInStock)
+        {
+            Title = inputTitle;
+            Author = inputAuthor;
+            YearOfPublication = inputYearOfPublication;
+            InStock = inputInStock;
+        }
+    }
+
 }
